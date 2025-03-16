83578f841646e1ca914d128f7f2a4576b8ad2f61
MBMigueis <migueiss2@hotmail.com>
2025-03-16 20:42:56 -0300

    aprendendo sobre template strings

---
+++ Atividades_JavaScript/Avaliacoes_JavaScript/template_strings/template_strings.js
@@ -0,0 +1,7 @@
+function imprimirOperacoes(a, b) {
+    console.log(`${a} + ${b} = ${a + b}`)
+    console.log(`${a} - ${b} = ${a - b}`)
+    console.log(`${a} x ${b} = ${a * b}`)
+    console.log(`${a} / ${b} = ${a / b}`)
+}
+imprimirOperacoes(4, 5)
